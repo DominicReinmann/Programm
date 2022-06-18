fb19bfa0e648afcab95632907bb923d0358b2615
Dominic Reinmann <reinmann.dominic@bluewin.ch>
2022-06-18 13:49:50 +0200

    4th commit

--- Subjects.py
+++ Subjects.py
@@ -1,2 +1,23 @@
 class Subjects:
-    subject = input()
+    sb = "Subject"
+    subjects = input("Enter subject ")
+    subject = subjects.capitalize()
+
+    if subject == "English" or "english":
+        print(f"{sb} {subject}")
+    elif subject == "German" or "german":
+        print(f"{sb} {subject}")
+    elif subject == "French" or "french":
+        print(f"{sb} {subject}")
+    elif subject == "Mathematics" or "mathematics":
+        print(f"{sb} {subject}")
+    elif subject == "Physics" or "physics":
+        print(f"{sb} {subject}")
+    elif subject == "Sport" or "sport":
+        print(f"{sb} {subject}")
+    elif subject == "Chemistry" or "chemistry":
+        print(f"{sb} {subject}")
+    elif subject == "Art" or "art":
+        print(f"{sb} {subject}")
+    else:
+        print("Please enter a valid Subject")
